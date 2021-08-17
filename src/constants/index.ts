3cfae3bf0464fe04918b7a78db043f4b02ebb272
Karthik X <karthik.x@314ecorp.com>
2021-08-17 13:16:11 +0530

    protocol fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,6 +2,6 @@
  * @author Karthik <karthik.x@314ecorp.com>
  * @description Constants
  */
-const BASE_URL = "http://www.omdbapi.com/";
+const BASE_URL = "https://www.omdbapi.com/";
 const APIKEY = "f28e42d";
 export { BASE_URL, APIKEY };
